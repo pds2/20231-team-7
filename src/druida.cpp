59a1ff670037fa0f1022acb9e4cff3cc4b0d0c14
CursinoCosta <105945436+CursinoCosta@users.noreply.github.com>
2023-06-06 09:44:25 -0300

    Add files via upload

---
+++ src/druida.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "../include/druida.h"
+
+   Druida::Druida(
+    string nome,
+    int vida,
+    int dano,
+    string classe
+):  Heroi(nome, vida, dano),
+     _trans(Humano) {}
+
+    
+    Classes Druida::get_classe() const{
+        return Classes::DRUIDA;
+    }
+
+    Tranformacao Druida::get_tranformacao(){
+        return _trans;
+
+    }
+
+    void Druida::transformar(Tranformacao trans){
+        if(_trans==trans) 
+            throw nao_pode_transformar_para_transformacao_atual_e();
+        _trans= trans;
+
+    }
+
+    void Druida::ataque(Personagem &inimigo1, Personagem &inimigo2, Personagem &inimigo3, int valor_dado){
+        if(get_nome() == inimigo1.get_nome()||get_nome() == inimigo2.get_nome()||get_nome() == inimigo3.get_nome())
+            throw personagem_ataca_a_si_mesmo_e();
+        if(valor_dado < 1)
+            throw valor_dado_negativo_e();
+        if(get_tranformacao()==Humano){
+            if(valor_dado == 1) inimigo1.recebe_dano(get_dano() - 3);
+            if(valor_dado == 2) inimigo1.recebe_dano(get_dano() - 2);
+            else if(valor_dado == 3) inimigo1.recebe_dano(get_dano());
+            else inimigo1.recebe_dano(get_dano() + (valor_dado / 2));
+        }
+        if(get_tranformacao()==Falcao){
+            if(valor_dado == 1) {
+                inimigo1.recebe_dano(get_dano() - 3);
+                inimigo2.recebe_dano(get_dano() - 3);
+                inimigo3.recebe_dano(get_dano() - 3);
+            }
+            if(valor_dado == 2) {
+                inimigo1.recebe_dano(get_dano() - 2);
+                inimigo2.recebe_dano(get_dano() - 2);
+                inimigo3.recebe_dano(get_dano() - 2);
+            }
+            else if(valor_dado == 3) {
+                inimigo1.recebe_dano(get_dano());
+                inimigo2.recebe_dano(get_dano());
+                inimigo2.recebe_dano(get_dano());
+            }
+            else {
+                inimigo1.recebe_dano(get_dano() + (valor_dado / 3));
+                inimigo2.recebe_dano(get_dano() + (valor_dado / 3));
+                inimigo3.recebe_dano(get_dano() + (valor_dado / 3));    
+            }
+        }
+        if(get_tranformacao()==Urso){
+            if(valor_dado == 1) inimigo1.recebe_dano(get_dano() - 1);
+            if(valor_dado == 2) inimigo1.recebe_dano(get_dano());
+            else if(valor_dado == 3) inimigo1.recebe_dano(get_dano()+1);
+            else inimigo1.recebe_dano(get_dano() + (3*valor_dado / 4));
+        }
+        if(get_tranformacao()==Lobo){
+            if(valor_dado == 1) {
+                inimigo1.recebe_dano(get_dano() - 2);
+                inimigo2.recebe_dano(get_dano() - 2);
+            }
+            if(valor_dado == 2) {
+                inimigo1.recebe_dano(get_dano() - 1);
+                inimigo2.recebe_dano(get_dano() - 1);
+            }
+            else if(valor_dado == 3) {
+                inimigo1.recebe_dano(get_dano());
+                inimigo2.recebe_dano(get_dano());
+            }
+            else {
+                inimigo1.recebe_dano(get_dano() + (valor_dado / 2));
+                inimigo2.recebe_dano(get_dano() + (valor_dado / 2));
+            }
+        }
+    }
---
+++ src/paladino.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "../include/paladino.h"
+
+   
+    Paladino::Paladino(
+    string nome, 
+    int vida,
+    int dano,
+    string classe
+    ):Heroi(nome, vida, dano, classe),
+    _mana(10), _manamax(10) {}
+    
+
+
+    
+    Classes Paladino::get_classe() const{
+        return PALADINO;
+    }
+
+
+    unsigned int Paladino::get_mana(){
+        return _mana;
+    }
+
+   
+    void Paladino::recuperar_mana(){
+        if(_mana<=(_manamax-2)) _mana=_mana+2;
+        if(_mana==(_manamax-1)) _mana++;
+    }
+
+    
+    void Paladino::magia_cura(Heroi &alvo, int valor_dado){
+
+
+    }
+
+   
+    void Paladino::ataque(Personagem &inimigo, int valor_dado){
+        
+    }
